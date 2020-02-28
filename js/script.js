8af407d551ea82b0f722ae2beb472b309fd4cf85
George Ngotho <davidmaryngotho@gmail.com>
2020-02-28 16:12:57 +0300

    added a document. ready in js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
+//Backend
+function PizzaPrice(pizzaSize,crustType,toppingsTotalPrice,quantity){
+	this.pizzaSize = pizzaSize,
+	this.crustType = crustType,
+	this.toppingsTotalPrice = toppingsTotalPrice,
+	this.quantity = quantity
+}
+
+PizzaPrice.prototype.calculatePizzaPrice = function() {
+	return this.quantity *(this.pizzaSize + this.crustType + this.toppingsTotalPrice)
+}
+
+
+
 $(document).ready( function ()
 {
+$('#pizz').submit(function(event){
+	let selectedPizzaSize = $("input[name = 'size']:checked").val()
+	let selectedPizzaCrust = $("input[name = 'crust']:checked").val()
+	console.log(selectedPizzaSize)
+
+
+	let selectedToppingPrices =[]
+	$("input[name='Sauce']:checked").each(()=>selectedToppingPrices.push(this.val()))
+	console.log(selectedToppingPrices.length)
+
+
+	event.preventDefault()
+})
   
-	$('#dropdown_selector').change(function()
-	{
+	// $('#dropdown_selector').change(function()
+	// {
 		
-		var option = $(this).find('option:selected').val();
+	// 	var option = $(this).find('option:selected').val();
 		
-		$('#showoption').val(option);
-	});
+	// 	$('#showoption').val(option);
+	// });
 });
